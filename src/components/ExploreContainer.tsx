a504b5d4ff679274a24dfa87b6a3fc264b3aa507
Psychohistorian11 <cristianfrancoraigosa@gmail.com>
2025-03-06 21:10:06 -0500

    android

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,16 @@
-import './ExploreContainer.css';
+import { IonButton, IonInput, IonItem, IonLabel } from "@ionic/react";
+import "./ExploreContainer.css";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonButton>Push me</IonButton>
+      <IonItem>
+        <IonLabel>Nombre</IonLabel>
+        <IonInput type="text" />
+      </IonItem>
     </div>
   );
 };
